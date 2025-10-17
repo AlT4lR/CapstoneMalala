e7290761afbd1f8072dee3cbe9aae88eabff3ef1
Altair <147609050+AlT4lR@users.noreply.github.com>
2025-10-17 18:18:50 +0800

    Version 2.8

--- Capstone/main.py
+++ Capstone/main.py
@@ -1,12 +1,7 @@
 import os
 from website import create_app
 
-# Get the configuration name from an environment variable, default to 'dev'
-config_name = os.getenv('FLASK_CONFIG') or 'dev'
-app = create_app(config_name)
+app = create_app(os.getenv('FLASK_CONFIG') or 'dev')
 
 if __name__ == '__main__':
-    # The debug=True flag is useful for development. 
-    # It enables the debugger and reloads the server when you make changes.
-    # For production, you would use a proper WSGI server instead of app.run().
     app.run(debug=True)
