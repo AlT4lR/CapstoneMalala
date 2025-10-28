5fdca91ce07ddfce2e00fbe94c621c92741327cb
Altair <147609050+AlT4lR@users.noreply.github.com>
2025-10-28 11:15:20 +0800

    Update build.sh

--- Capstone/build.sh
+++ Capstone/build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-apt-get update
+
 apt-get install -y tesseract-ocr tesseract-ocr-eng
 
+# After the system package is installed, we install the Python packages.
 pip install -r requirements.txt
