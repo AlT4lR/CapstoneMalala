f22122af13d8af90d71b569febe8087659010975
Altair <147609050+AlT4lR@users.noreply.github.com>
2025-10-28 11:06:06 +0800

    Update build.sh

--- Capstone/build.sh
+++ Capstone/build.sh
@@ -2,9 +2,7 @@
 # exit on error
 set -o errexit
 
-# --- START OF MODIFICATION ---
-sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
-# --- END OF MODIFICATION ---
+apt-get update
+apt-get install -y tesseract-ocr tesseract-ocr-eng
 
-# Install Python dependencies from requirements.txt
 pip install -r requirements.txt
